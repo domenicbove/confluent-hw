c9f5af3e7383d702ace93442b03c415c5fb31f35
Domenic Bove <domenicbove@gmail.com>
2019-07-11 10:03:39 -0700

    some comments

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -5,6 +5,10 @@ variable "secret_key" {}
 variable "region" {
   default = "us-west-1"
 }
+## us-west-1 centos ami
+variable "centos_ami" {
+  default = "ami-074e2d6769f445be5"
+}
 # There is a way to use a data object to query this
 # variable "rhel_ami" {
 #   default = "ami-28e07e50"
@@ -12,9 +16,6 @@ variable "region" {
 # variable "ubuntu_ami" {
 #   default = "ami-0ab6b07a01e922253"
 # }
-variable "centos_ami" {
-  default = "ami-074e2d6769f445be5"
-}
 variable "ec2_type_master" {
   default = "t2.medium"
 }
